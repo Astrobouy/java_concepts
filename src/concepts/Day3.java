3f2576793d8c3e79e892f38b67fbd6b5400065c9
Ashton Williams <ashtonwilliams82895@gmail.com>
2023-12-02 18:55:10 -0500

    refactoring

--- src/concepts/Day3.java
+++ src/concepts/Day3.java
@@ -3,8 +3,10 @@ package concepts;
 public class Day3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int age = 25;
+		String name = "Skill Captain";
+		
+		System.out.println("Hello, " + name + "! " + "You are " + age + " years old.");
 
 	}
-
 }
