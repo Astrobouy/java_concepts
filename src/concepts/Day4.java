98e4e93d5e84a695f4e85e7df9c42639be4c1a3f
Ashton Williams <ashtonwilliams82895@gmail.com>
2023-12-02 21:38:14 -0500

    updating files

---
+++ src/concepts/Day4.java
@@ -0,0 +1,14 @@
+package concepts;
+
+public class Day4 {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 20;
+		
+		System.out.println("For example, with num1 set to " + num1 + " and num2 set to " + num2 + ", your program should output:");
+		System.out.println("The larger number is: " + (num1 > num2? num1:num2));
+
+	}
+
+}
