e253f7c4502eff85e77226509c22c0d8c477b7df
Ashton Williams <ashtonwilliams82895@gmail.com>
2023-12-03 14:53:10 -0500

    Add day7

---
+++ src/concepts/Day7.java
@@ -0,0 +1,27 @@
+package concepts;
+
+public class Day7 {
+
+	public static void main(String[] args) {
+		int [] intArray = new int []{11, 10, 16, 5, 20};
+		int sum = 0;
+		double mean = 0.0;
+		int maxNumber = intArray[0];
+		int minNumber = intArray[0];
+		
+		for(int i = 0; i < intArray.length; i++) {
+			sum+= intArray[i];
+			if(maxNumber < intArray[i]) {
+				maxNumber = intArray[i];
+			}
+			if(minNumber > intArray[i]) {
+				minNumber = intArray[i];
+			}
+		}
+		mean = (double)sum/intArray.length;
+		System.out.println("Sum: " + sum);
+		System.out.println("Average: " + mean);
+		System.out.println("Maximum: " + maxNumber);
+		System.out.println("Minimum: " + minNumber);
+	}
+}
