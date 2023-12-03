6967d8d77b6ba31b22dcb5363f53ff1964d62c77
Ashton Williams <ashtonwilliams82895@gmail.com>
2023-12-03 13:30:28 -0500

    Updated calculator

--- src/concepts/Day5.java
+++ src/concepts/Day5.java
@@ -6,11 +6,18 @@ public class Day5 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("what would you like to calculate?");
-		int num1 = scanner.nextInt();
-		char operator = scanner.next().charAt(0);
-		int num2 = scanner.nextInt();
 		
-		System.out.println("" + num1 + " " + operator + " " + num2 + " = " + Calculator(num1, operator, num2));
+		if(scanner.hasNextInt()) {
+			int num1 = scanner.nextInt();
+			char operator = scanner.next().charAt(0);
+			int num2 = scanner.nextInt();
+			System.out.println("" + num1 + " " + operator + " " + num2 + " = " + Calculator(num1, operator, num2));
+		}else {
+			System.out.println("Not a number");
+		}
+		
+		
+		scanner.close();
 	}
 	
 	public static int Calculator(int num1, char operator, int num2) {
@@ -30,6 +37,10 @@ public class Day5 {
 			
 		case '/':
 			result = num1 / num2;
+			break;
+			
+		default:
+			System.out.println("Incorrect input");
 			break;
 		}
 		return result;	
